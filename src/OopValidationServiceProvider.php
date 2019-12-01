886b3b4e26f087e3ec9c0d3c7894cbacda11e1f1
Hardik G Prajapati <prajapati.hardik18@gmail.com>
2019-12-02 00:00:43 +0530

    Add servicde provider

---
+++ src/OopValidationServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace gmsth\OopsValidator;
+
+use Illuminate\Support\ServiceProvider;
+
+class OopsValidatorServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+    }
+    public function register()
+    {
+    }
+}
